87f7bee6c6b90f0a2166570e959127eaf3466702
Bhavana N K <1nc23cs030@ncetmail.com>
2026-07-29 15:05:28 +0530

    Time: 0 ms (100%), Space: 9.1 MB (7.04%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_map<char,int> freq;
+        for(int i=0;i<sentence.size();i++)
+        {
+            freq[sentence[i]]++;
+        }
+        if(freq.size()==26) return true;
+    return false;
+    }
+};
